883c9970d038f0032e973d2283a7800a6dd75015
renanpablo717 <renanpablo717@gmail.com>
2026-07-19 10:53:37 -0300

    Update testecat.py

--- consumo APIS/testecat.py
+++ consumo APIS/testecat.py
@@ -1,18 +1,15 @@
 import requests
 from translate import Translator
-
 url = "https://catfact.ninja/fact"
 response = requests.get(url)
-
 dados = response.json()
 
-# 1. Pegamos apenas o texto em inglês que está dentro da chave 'fact'
+# Pega o texto em inglês que está dentro da chave 'fact'
 fato_em_ingles = dados.get('fact')
 
-# 2. Configurando o tradutor de Inglês (en) para Português (pt)
+# 2. Configura o tradutor de Inglês (en) para Português (pt)
 translator = Translator(from_lang="en", to_lang="pt")
 
-# 3. Passamos a STRING para o tradutor
+# 3. Passa a STRING para o tradutor
 translation = translator.translate(fato_em_ingles)
-
 print(f"Fato do gato: {translation}")
