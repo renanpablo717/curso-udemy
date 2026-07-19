88b8f707883b59213a0d02b9ac4d783b1b36a7fe
renanpablo717 <renanpablo717@gmail.com>
2026-07-19 09:58:27 -0300

    retornos de solictações

--- consumo APIS/propriedadesresponses-1.py
+++ consumo APIS/propriedadesresponses-1.py
@@ -3,3 +3,15 @@ import requests
 resp = requests.get("https://google.com")
 
 print(resp.status_code)
+
+if resp.status_code < 400:
+    #Seguir com o código
+elif resp.status_code >= 400 and resp.status_code < 500:
+    #Corrigir solicitação do cliente
+    # 400 erro na solicitação
+    # 401 Falta de autenticação
+    # 403 Autenticação negada
+    # 404 Recurso não encontrado
+    # 405 Método não permitido(GET onde só pode POST)
+    # 429 Requisições demasiadas
+    print("Erro do cliente")
